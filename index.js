f80de3697798f3cf916734554eebb6416ea54463
Ilham el gharbi <119396809+Ramo2001@users.noreply.github.com>
2025-04-25 11:36:31 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+
+  const toggleBtn = document.getElementById('menu-toggle');
+  const navbar = document.getElementById('navbar');
+
+  toggleBtn.addEventListener('click', () => {
+    navbar.style.display = navbar.style.display === 'block' ? 'none' : 'block';
+  });
+
+
+
+
